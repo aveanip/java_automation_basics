c81de91034b6e93ae7049ec3f044fddeaf1b6709
Anastasia <aveanip@yandex.ru>
2026-06-03 12:49:48 +0300

    Fixed tests

--- src/test/java/Conflict.java
+++ src/test/java/Conflict.java
@@ -1,9 +1,9 @@
 public class Conflict {
-    public static void conflictTest(){
+    public static void conflictTest() {
         System.out.println(getMessage());
     }
 
-    private static String getMessage(){
+    private static String getMessage() {
         return "Hello World!";
     }
 }
