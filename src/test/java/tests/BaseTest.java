904b0b7ca7ce03e91834c8d8d338923b2e3d9a2e
Anastasia <aveanip@yandex.ru>
2026-08-08 16:51:27 +0300

    Добавления в build.gradle

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,10 +18,16 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion= "149.0";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
     void afterEach() {
         closeWebDriver();
     }
+//    @AfterEach
+//    void addAttachments(){
+//        Attach.screenshotAs("Last screenshot");
+//    }
 }
