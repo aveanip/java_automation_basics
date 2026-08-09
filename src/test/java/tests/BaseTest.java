3cd0217fb28f41f0d07c1a01661d235b89a717d1
Anastasia <aveanip@yandex.ru>
2026-08-09 17:08:59 +0300

    Добавления в BaseTest Configuration selenoidUrl

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -37,7 +37,7 @@ public class BaseTest {
         if (selenoidUrl == null || selenoidUrl.isEmpty() || "null".equals(selenoidUrl)) {
             selenoidUrl = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         }
-        //        Configuration.remote = "https://user1:1234@selenoid.qa.guru/wd/hub";
+        Configuration.remote = selenoidUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
